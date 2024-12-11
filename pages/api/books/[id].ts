53ffc220126dc1236406d6e37649da7dd7b3b9ab
gunyongbok <qhrrjsdyd123@gmail.com>
2024-12-11 13:54:47 +0900

    feat: 개별 책 정보 수정 api 구현

--- pages/api/books/[id].ts
+++ pages/api/books/[id].ts
@@ -22,8 +22,6 @@ export default async function handler(
           .status(404)
           .json({ error: "해당 id를 가진 책은 존재하지 않습니다." });
       }
-
-      // 책 정보 반환
       res.status(200).json(books[0]);
     } catch (error) {
       res.status(500).json({ error: "데이터베이스 쿼리 실패" });
@@ -37,7 +35,6 @@ export default async function handler(
     }
 
     try {
-      // 책이 존재하는지 확인
       const [rows] = await pool.query("SELECT * FROM books WHERE id = ?", [id]);
 
       if ((rows as Book[]).length === 0) {
@@ -46,15 +43,47 @@ export default async function handler(
           .json({ error: "해당 id를 가진 책은 존재하지 않습니다." });
       }
 
-      // 책 삭제
       await pool.query("DELETE FROM books WHERE id = ?", [id]);
 
       res.status(200).json({ message: "책이 성공적으로 삭제되었습니다." });
     } catch (error) {
       res.status(500).json({ error: "데이터베이스 쿼리 실패" });
     }
+  }
+
+  // 책 수정
+  else if (req.method === "PUT") {
+    if (!id) {
+      return res.status(400).json({ error: "책 id가 없습니다." });
+    }
+
+    const { title, author, stock } = req.body;
+
+    if (!title || !author || !stock) {
+      return res.status(400).json({ error: "모든 필드를 입력해야 합니다." });
+    }
+
+    try {
+      const [rows] = await pool.query("SELECT * FROM books WHERE id = ?", [id]);
+
+      if ((rows as Book[]).length === 0) {
+        return res
+          .status(404)
+          .json({ error: "해당 id를 가진 책은 존재하지 않습니다." });
+      }
+
+      // 책 정보 수정
+      await pool.query(
+        "UPDATE books SET title = ?, author = ?, stock = ? WHERE id = ?",
+        [title, author, stock, id]
+      );
+
+      res.status(200).json({ message: "책 정보가 성공적으로 수정되었습니다." });
+    } catch (error) {
+      res.status(500).json({ error: "데이터베이스 쿼리 실패" });
+    }
   } else {
-    res.setHeader("Allow", ["GET", "DELETE"]);
+    res.setHeader("Allow", ["GET", "DELETE", "PUT"]);
     res.status(405).end("허용되지 않은 응답 처리입니다.");
   }
 }
