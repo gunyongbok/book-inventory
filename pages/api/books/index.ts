05bafeae1e7cba251c2cb8486a0f8fd9c9acd221
gunyongbok <qhrrjsdyd123@gmail.com>
2024-12-11 13:27:04 +0900

    feat: 동일한 책 제목과 작가의 책을 추가하는 경우 새로운 기존의 책 수량에 추가해주는 방식 기능 추가

--- pages/api/books/index.ts
+++ pages/api/books/index.ts
@@ -25,11 +25,35 @@ export default async function handler(
     }
 
     try {
-      await pool.query(
-        "INSERT INTO books (title, author, stock) VALUES (?, ?, ?)",
-        [title, author, stock]
+      // 기존에 동일한 책이 있는지 확인하는 쿼리
+      const [rows] = await pool.query(
+        "SELECT * FROM books WHERE title = ? AND author = ?",
+        [title, author]
       );
-      res.status(201).json({ message: "책이 성공적으로 추가되었습니다." });
+
+      if ((rows as Book[]).length > 0) {
+        // 책이 이미 존재하면 수량만 업데이트
+        const bookId = (rows as Book[])[0].id;
+        const newStock = (rows as Book[])[0].stock + stock;
+
+        await pool.query("UPDATE books SET stock = ? WHERE id = ?", [
+          newStock,
+          bookId,
+        ]);
+
+        return res.status(200).json({
+          message: "책이 이미 존재하여 수량이 업데이트되었습니다.",
+        });
+      } else {
+        // 책이 없으면 새로 추가
+        await pool.query(
+          "INSERT INTO books (title, author, stock) VALUES (?, ?, ?)",
+          [title, author, stock]
+        );
+        return res
+          .status(201)
+          .json({ message: "책이 성공적으로 추가되었습니다." });
+      }
     } catch (error) {
       res.status(500).json({ error: "데이터베이스 쿼리 실패" });
     }
