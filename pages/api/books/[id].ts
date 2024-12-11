9fcd0106e52c8aebe72caef903a8f3dfa45b888a
gunyongbok <qhrrjsdyd123@gmail.com>
2024-12-11 13:51:19 +0900

    feat: 책 삭제 기능 api 구현

--- pages/api/books/[id].ts
+++ pages/api/books/[id].ts
@@ -8,6 +8,7 @@ export default async function handler(
 ) {
   const { id } = req.query;
 
+  // 책 상세 조회
   if (req.method === "GET") {
     if (!id) {
       return res.status(400).json({ error: "책 id가 없습니다." });
@@ -27,9 +28,33 @@ export default async function handler(
     } catch (error) {
       res.status(500).json({ error: "데이터베이스 쿼리 실패" });
     }
+  }
+
+  // 책 삭제
+  else if (req.method === "DELETE") {
+    if (!id) {
+      return res.status(400).json({ error: "책 id가 없습니다." });
+    }
+
+    try {
+      // 책이 존재하는지 확인
+      const [rows] = await pool.query("SELECT * FROM books WHERE id = ?", [id]);
+
+      if ((rows as Book[]).length === 0) {
+        return res
+          .status(404)
+          .json({ error: "해당 id를 가진 책은 존재하지 않습니다." });
+      }
+
+      // 책 삭제
+      await pool.query("DELETE FROM books WHERE id = ?", [id]);
+
+      res.status(200).json({ message: "책이 성공적으로 삭제되었습니다." });
+    } catch (error) {
+      res.status(500).json({ error: "데이터베이스 쿼리 실패" });
+    }
   } else {
-    // GET 이외의 요청이 들어왔을 경우 405 Method Not Allowed 반환
-    res.setHeader("Allow", ["GET"]);
-    res.status(405).end("Get 요청만 가능합니다.");
+    res.setHeader("Allow", ["GET", "DELETE"]);
+    res.status(405).end("허용되지 않은 응답 처리입니다.");
   }
 }
