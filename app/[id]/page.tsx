347b4e71f9aa438d96389d13d0b334a12bea922b
gunyongbok <qhrrjsdyd123@gmail.com>
2024-12-12 12:57:08 +0900

    fix: 배포 오류 수정

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import BookDetails from "./BookDetails";
 
-const BookPage = async ({ params }: { params: { id: string } }) => {
+const BookPage = ({ params }: { params: { id: string } }) => {
   return (
     <Suspense fallback={<div>로딩 중...</div>}>
       <BookDetails id={params.id} />
