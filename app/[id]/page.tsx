c8b01d4ca883114d5a8fbfc348faa02159558287
gunyongbok <qhrrjsdyd123@gmail.com>
2024-12-12 13:04:56 +0900

    fix: use client 추가

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,28 +1,21 @@
-import { Suspense, useEffect, useState } from "react";
+"use client";
+
+import { Suspense } from "react";
+import { use } from "react";
 import BookDetails from "./BookDetails";
 
 type PageProps = {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 };
 
 const BookPage = ({ params }: PageProps) => {
-  const [id, setId] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (params.id) {
-      setId(params.id);
-    }
-  }, [params]);
-
-  if (!id) {
-    return <div>로딩 중...</div>;
-  }
+  const unwrappedParams = use(params);
 
   return (
     <Suspense fallback={<div>로딩 중...</div>}>
-      <BookDetails id={id} />
+      <BookDetails id={unwrappedParams.id} />
     </Suspense>
   );
 };
